04cfd15d750e2760e3f1fbe6686552b329a748eb
FaizahMalik <78925690+FaizahMalik@users.noreply.github.com>
2023-08-09 11:36:03 +0100

    adding new maps and debugging

--- misc/map_screencapper.py
+++ misc/map_screencapper.py
@@ -5,8 +5,8 @@ import os
 # from functools import partial
 
 timeBetweenScreenshots = 30 # in seconds
-screenshotPath = '/home/suh/Pictures/Testing/03/mrgs_hypo_60'
-screenshotPrefix = '03_60_mrgs'
+screenshotPath = '/home/suh/Pictures/Testing/05/mrgs'
+screenshotPrefix = '05_mrgs'
 xStart = 1965
 width = 2560-(xStart - 1920)
 yStart = 30
